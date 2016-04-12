0dead657b88595394824faf71500704bb90b7d47
Menglai Wang <wangmenglai91@gmail.com>
2016-04-12 12:29:47 +0800

    Added files via upload

--- extractOpticalFlow_gpu.py
+++ extractOpticalFlow_gpu.py
@@ -1,22 +1,27 @@
 import glob
 import os
 #import pdb
-path1 = '/home/zyy/workspace/wangml/caffe-action/data/PersonRunsVideo/CAM3/'
-path2 = '/home/zyy/workspace/wangml/caffe-action/data/PersonRuns_Flow_Img/'
+path_video = '/home/zyy/workspace/wangml/caffe-action/data/PersonRunsVideo/CAM1/'
+path_rgb = '/home/zyy/workspace/wangml/caffe-action/data/PersonRuns_rgb_img/'
+path_flow = '/home/zyy/workspace/wangml/caffe-action/data/PersonRuns_flow_img/'
 
 videolist = []
-videolist = glob.glob(path1 + '/*')
+videolist = glob.glob(path_video + '/*')
 videoname = [fn.split('/',9)[9] for fn in videolist]
 
 for v in videoname:
     v = v.split('.',1)[0]
-    path = os.path.join(path2 , v)
-    if not os.path.exists(path):
-        os.mkdir(path)
-    file1 = path1 + v + '.avi'
-    file2 = path + '/flow_x'
-    file3 = path + '/flow_y'
-    file4 = path + '/flow_i'
+    path1 = os.path.join(path_rgb , v)
+    if not os.path.exists(path1):
+        os.mkdir(path1)
+
+    path2 = os.path.join(path_flow , v)
+    if not os.path.exists(path2):
+        os.mkdir(path2)
+    file1 = path_video + v + '.avi'
+    file2 = path2 + '/flow_x'
+    file3 = path2 + '/flow_y'
+    file4 = path1 + '/image'
     device_id = 0
     step = 1
     cmd = './denseFlow_gpu -f {} -x {} -y {} -i {} -b 20 -t {} -d {} -s {}'.format( \
